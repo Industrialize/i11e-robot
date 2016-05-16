164d05bb921004cfc6ab3528829acb5e4de07f30
Bo HOU <bhoustudio@gmail.com>
2016-05-17 01:39:12 +0200

    init commit

---
+++ test/test.js
@@ -0,0 +1,43 @@
+exports['test Robot'] = {
+  'test robot': (test) => {
+    var createRobot = require('../lib/index').createRobot;
+    var Box = require('i11e-box');
+    var Robot = createRobot({
+      input() {
+        return {
+          "a:&": 10,
+          "b:&": 20
+        };
+      },
+
+      output() {
+        return {
+          c: (v, box) => {
+            return v === box.get('a') + box.get('b') * this.options.k;
+          },
+        };
+      },
+
+      process(box, done) {
+        const a = box.get('a');
+        const b = box.get('b');
+
+        box.set('c', a + b * this.options.k);
+
+        done(null, box);
+      }
+    });
+
+    var robot = new Robot({
+      k: 10
+    });
+
+    robot.process(new Box({
+      a: 1,
+      b: 2,
+    }), function(err, box) {
+      test.equal(box.get('c'), 21);
+      test.done();
+    });
+  }
+}
